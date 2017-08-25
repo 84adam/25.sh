c87249b84a8e715473c6d217fc58943e9d88ebcb
Adam Anderson <84adam@users.noreply.github.com>
2017-08-24 18:10:19 -0700

    Printf column formatting, +BTC +24hr +7day

--- 25.sh
+++ 25.sh
@@ -1,47 +1,34 @@
 #!/bin/bash
 
-# 25.sh -- Top 25 Cryptocurrencies by Market Cap -- Via coinmarketcap.com 
+# DOWNLOAD TOP 25 CRYPTOCURRENCY PRICE DATA FROM COINMARKETCAP.COM USING 'curl'
 
-# ******************************* GET DATA USING CURL *************************************** #
-# Grab Top 25 Coins from https://api.coinmarketcap.com/v1/ticker/?limit=25
-# Output to 'top25.json' file in current directory
+headerSRC="%7s %50s %5s"
+printf "\n"
+printf "$headerSRC" "/////" "Downloading the latest price data from http://coinmarketcap.com ..." "/////"
+printf "\n"
 
-echo "/// ... UPDATING PRICES ... ///"
-sleep 0.5
-echo " "
 curl --silent 'https://api.coinmarketcap.com/v1/ticker/?limit=25' > top25.json
 
-# ******************************* PARSE DATA USING JQ *************************************** #
-# See 'jq' manual here: https://stedolan.github.io/jq/manual/
-# Filter, map JSON data from top25.json file to arrays:
-# .name -> 'aNAME'; .symbol -> 'aSYM'; .price_usd -> 'aUSD'; '.market_cap.usd' -> 'aMCAP'
+# PARSE JSON DATA INTO ARRAYS USING 'jq'
 
+mapfile -t aSYM  < <( jq -r '.[] | .symbol' top25.json )
 mapfile -t aNAME < <( jq -r '.[] | .name' top25.json )
-mapfile -t aSYM < <( jq -r '.[] | .symbol' top25.json )
-mapfile -t aUSD < <( jq -r '.[] | .price_usd' top25.json )
+mapfile -t aUSD  < <( jq -r '.[] | .price_usd' top25.json )
 mapfile -t aMCAP < <( jq -r '.[] | .market_cap_usd' top25.json )
+mapfile -t aBTC  < <( jq -r '.[] | .price_btc' top25.json )
+mapfile -t aP24H < <( jq -r '.[] | .percent_change_24h' top25.json )
+mapfile -t aP7D  < <( jq -r '.[] | .percent_change_7d' top25.json )
 
-# **************************** PRINT DATA USING PRINTF ************************************** #
-# Print output, iterating through each 25-item array using 'seq'; number line outputs with 'nl'
+# DISPLAY TOP 25 COINS BY MARKET CAP
 
+header="%7s %-17s %-6s %-12s %-17s %-13s %11s %12s\n"
+format="%-17s %-6s %-1s %-10s %-1s %13s %3s %-10s %12s %12s\n"
+printf "\n"
+printf "$header" "RANK" "COIN" "SYMBOL" "PRICE" "MARKET CAP" "BTC PRICE" "24HR CH" "7DAY CH"
+printf "\n"
 for var in `seq 0 24`;
-do printf "${aNAME[$var]} (${aSYM[$var]}) - \$${aUSD[$var]} USD - \$${aMCAP[$var]} MCAP\n";
+do printf "$format" "${aNAME[$var]}" "${aSYM[$var]}" "\$" "${aUSD[$var]}" "\$" "${aMCAP[$var]}" "B" "${aBTC[$var]}" "${aP24H[$var]} pct" "${aP7D[$var]} pct";
 done | nl -s ". "
-
-# ************************* API/SOURCE DATA ATTRIBUTION ************************************* #
-
-echo " "
-echo "VIA: http://coinmarketcap.com"
-echo " "
-
-# ************************************** ROADMAP ******************************************** #
-#
-# 1. Improve number formatting: K, M, B, T (up to 'Trillions')
-#    'awk' example:
-#
-#     awk 'BEGIN{ u[0]=""; u[1]="K"; u[2]="M"; u[3]="B"; u[4]="T"} 
-#     { n = $1; i = 0; while(n > 1000) { i+=1; n= int(n/1000) } print n u[i] } '
-#     <array-or-log-filename> | ns -s ". "
-#
-# 2. Round numbers up/down using 'bc'
-#    For example: so that '1499999.99' becomes '1,500,000' (or '1.5M')
+printf "\n"
+printf "$header" " " "COIN" "SYMBOL" "PRICE" "MARKET CAP" "BTC PRICE" "24HR CH" "7DAY CH"
+printf "\n"
